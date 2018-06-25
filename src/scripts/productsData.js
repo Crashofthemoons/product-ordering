a4ea333b044fe90d089c17dc4d6e79f89c30077c
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-06-25 09:45:18 -0500

    started database

---
+++ src/scripts/productsData.js
@@ -0,0 +1,42 @@
+let Products = Object.create({}, {
+    value: {
+        1: {
+            title: "Cheese",
+            img: "https://oukosher.org/content/uploads/2018/05/Swiss_Cheese_Garden_of_Eden.jpg",
+            description: "Lorem ipsum dolor amet seitan chambray single-origin coffee hot chicken, wolf 8-bit letterpress craft beer iPhone mlkshk. Swag jianbing venmo butcher stumptown palo santo neutra migas ethical. Mustache typewriter meditation venmo sustainable intelligentsia waistcoat bushwick jean shorts slow-carb wolf normcore viral DIY. Photo booth trust fund gochujang plaid la croix thundercats occupy marfa butcher.",
+            id: 1
+        },
+        2: {
+            title: "Bread",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_xqHfiPCEKHgkoj1VwqZBxgK-kEWrsns9WiJPsetoRFta4JYP",
+            description: "Street art shabby chic butcher, chia quinoa hell of tacos vaporware kickstarter pug 3 wolf moon deep v hashtag gentrify. Cred bicycle rights flexitarian synth blog food truck bitters vexillologist gluten-free pok pok whatever. Pitchfork DIY pop-up etsy, vegan pinterest distillery sustainable prism disrupt shabby chic VHS listicle. Blue bottle put a bird on it yr cray vape organic.",
+            id: 2
+        },
+        3: {
+            title: "Tree",
+            img: "https://img2.cgtrader.com/items/710237/9776ec3822/oak-tree-3d-model-max-obj.jpg",
+            description: "Enamel pin migas banh mi, kickstarter af chia trust fund. Celiac hexagon vice pop-up master cleanse mixtape gentrify, blue bottle mlkshk drinking vinegar. Butcher taxidermy VHS blog farm-to-table, asymmetrical mustache umami live-edge meggings ramps glossier godard sartorial organic. Portland direct trade wayfarers tofu cronut church-key semiotics.",
+            id: 3
+        },
+        4: {
+            title: "Coconut",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSytpC5QlsfyHJ_cosIJ-iDeIxgWUfHhPay7oMES9T_nh4D4N_t",
+            description: "Humblebrag sustainable copper mug slow-carb godard celiac. Direct trade waistcoat four dollar toast bicycle rights man braid poke knausgaard. PBR&B whatever edison bulb cliche vexillologist intelligentsia, trust fund enamel pin wolf chartreuse four dollar toast woke.",
+            id: 4
+        },
+        5: {
+            title: "Milk",
+            img: "https://cdn-image.realsimple.com/sites/default/files/styles/rs_horizontal_image_4/public/milk-bottle.jpg?itok=-XfTzxJk",
+            description: "Cred church-key schlitz copper mug jianbing selvage hoodie poke kitsch mumblecore. Heirloom chicharrones man braid pork belly etsy, pok pok tumeric asymmetrical man bun. Cliche disrupt literally, brooklyn cardigan fam shaman copper mug waistcoat tbh pour-over. ",
+            id: 5
+        },
+        6: {
+            title: "Croissant",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIoNkDex5jCsJo1E3Xxn_fq4bjY3iZpOJEiIDFrCzytSTFsHNu",
+            description: "Humblebrag sustainable copper mug slow-carb godard celiac. Direct trade waistcoat four dollar toast bicycle rights man braid poke knausgaard. PBR&B whatever edison bulb cliche vexillologist intelligentsia, trust fund enamel pin wolf chartreuse four dollar toast woke.",
+            id: 6
+        },
+        writable: true,
+        enumerable: true
+    }
+})
---
+++ src/scripts/reviewsData.js
@@ -0,0 +1,42 @@
+let Reviews = Object.create({}, {
+    value: {
+        123: {
+            value: "Thundercats meh hot chicken letterpress mlkshk, lyft tacos +1 celiac pork belly chicharrones raclette irony try-hard offal. 8-bit organic farm-to-table iPhone fam bicycle rights narwhal XOXO meditation. Church-key deep v organic, 8-bit forage health goth vexillologist trust fund readymade. Cliche plaid poke, normcore live-edge umami seitan. PBR&B kinfolk vinyl, +1 fanny pack vexillologist green juice bicycle rights chicharrones tilde godard cloud bread tumblr whatever. Jean shorts chillwave schlitz dreamcatcher XOXO yr four loko selfies poke next level marfa aesthetic.",
+            stars: 3,
+            productId: 1,
+            id: 123
+        },
+        234: {
+            value: "Actually jean shorts live-edge hella, skateboard bicycle rights blue bottle distillery tote bag banjo pour-over viral cloud bread. Photo booth truffaut direct trade lyft narwhal deep v PBR&B wolf viral occupy locavore mumblecore. Bitters drinking vinegar chia, kickstarter meggings kale chips air plant tattooed DIY pinterest. Kitsch plaid bitters sartorial vexillologist, cred kogi meditation enamel pin hammock offal cronut everyday carry.",
+            stars: 4,
+            productId: 1,
+            id: 234
+        },
+        345: {
+            value: "Bushwick polaroid enamel pin franzen, sriracha keffiyeh unicorn deep v microdosing. Yr venmo blue bottle chartreuse, tumeric food truck listicle sartorial actually beard. Ennui roof party live-edge ethical godard chia vexillologist selvage four dollar toast af typewriter +1 occupy. Celiac thundercats air plant sartorial la croix food truck. Butcher master cleanse four dollar toast listicle ennui portland gochujang cray ugh normcore franzen.",
+            stars: 1,
+            productId: 1,
+            id: 345
+        },
+        567: {
+            value: "Thundercats meh hot chicken letterpress mlkshk, lyft tacos +1 celiac pork belly chicharrones raclette irony try-hard offal. 8-bit organic farm-to-table iPhone fam bicycle rights narwhal XOXO meditation. Church-key deep v organic, 8-bit forage health goth vexillologist trust fund readymade. Cliche plaid poke, normcore live-edge umami seitan. PBR&B kinfolk vinyl, +1 fanny pack vexillologist green juice bicycle rights chicharrones tilde godard cloud bread tumblr whatever. Jean shorts chillwave schlitz dreamcatcher XOXO yr four loko selfies poke next level marfa aesthetic.",
+            stars: 3,
+            productId: 2,
+            id: 567
+        },
+        456: {
+            value: "Actually jean shorts live-edge hella, skateboard bicycle rights blue bottle distillery tote bag banjo pour-over viral cloud bread. Photo booth truffaut direct trade lyft narwhal deep v PBR&B wolf viral occupy locavore mumblecore. Bitters drinking vinegar chia, kickstarter meggings kale chips air plant tattooed DIY pinterest. Kitsch plaid bitters sartorial vexillologist, cred kogi meditation enamel pin hammock offal cronut everyday carry.",
+            stars: 4,
+            productId: 2,
+            id: 456
+        },
+        678: {
+            value: "Bushwick polaroid enamel pin franzen, sriracha keffiyeh unicorn deep v microdosing. Yr venmo blue bottle chartreuse, tumeric food truck listicle sartorial actually beard. Ennui roof party live-edge ethical godard chia vexillologist selvage four dollar toast af typewriter +1 occupy. Celiac thundercats air plant sartorial la croix food truck. Butcher master cleanse four dollar toast listicle ennui portland gochujang cray ugh normcore franzen.",
+            stars: 1,
+            productId: 2,
+            id: 678
+        },
+        writable: true,
+        enumerable: true
+    },
+})
